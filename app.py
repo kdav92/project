2f37b3ced0b7cfda38276ff8a830a383aaef9bec
Kelcey <klcy_dav@yahoo.com>
2025-03-07 21:20:31 +0900

    New Updates

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
-import flask from Flask, request, jsonify
+from flask import Flask, render_template, jsonify
 import sqlite3
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/survey", methods=["GET", "POST"])
+def survey():
+    return render_template("survey.html")
+
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
