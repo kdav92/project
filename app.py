4fc333d23980ff5e66b7b77c902eaa9ef1d0f2da
Kelcey <klcy_dav@yahoo.com>
2025-03-09 18:14:44 +0900

    new updates to files

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
+
 app = Flask(__name__)
+
+db = SQL("sqlite3:///questions.db")
 
 
 @app.route("/")
@@ -13,10 +16,16 @@ def index():
 def survey():
     return render_template("survey.html")
 
+@app.route("/sumbit", methods=["GET", "POST"])
+def submit():
+    return render_template("results.html")
+
 
 @app.route("/results")
 def results():
     return render_template("results.html")
+
+
 
 
 if __name__ == "__main__":
